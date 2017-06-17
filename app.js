298779756556105f9f9a69303ce7436f6c7d6bf6
“[josue <[josuerojas.rojas@gmail.com]>
2017-06-17 18:18:58 -0400

    try

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.use(express.static( "public" )); //access pictures and suchs
 
 app.use(parser.json());
 //connect to database
-var connectionString = 'postgres://qztjolhrzsotrl:4f88f9621ee0de6a4cb1f161119f6d7885b37091aaa41428b21d5c6f0e504097@ec2-184-73-167-43.compute-1.amazonaws.com:5432/d462qipcfp5r43'
-//var connectionString = "postgres://postgres:1234567890@localhost:5432/portfolio";
+var connectionString = "postgres://postgres:1234567890@localhost:5432/portfolio";
 var pgClient = new pg.Client(connectionString);
 pgClient.connect();
 
@@ -28,7 +27,7 @@ console.log("inside app.js");
 //pgClient.query("CREATE TABLE IF NOT EXISTS blog(id SERIAL UNIQUE PRIMARY KEY, title varchar(255) NOT NULL, date date NOT NULL default CURRENT_DATE, summary text NOT NULL, body text NOT NULL)")
 //pgClient.query("CREATE TABLE IF NOT EXISTS comment(id SERIAL UNIQUE PRIMARY KEY,date date NOT NULL default CURRENT_DATE, name varchar(255) NOT NULL, email varchar(255) NOT NULL, comment text NOT NULL, postID Integer NOT NULL REFERENCES blog(id))");
 //pgClient.query("CREATE TABLE IF NOT EXISTS projects(id SERIAL UNIQUE PRIMARY KEY, title varchar(255) NOT NULL, date date NOT NULL, url varchar(255) NOT NULL, languages varchar(255) NOT NULL, info text NOT NULL)")
-//pgClient.query("INSERT INTO projects(title, date, url,languages, info) values($1,$2,$3,$4,$5)",["Data Allocation","2016-11-29","https://github.com/josuerojasrojas/DataAllocation","Java","This project was for OS Class. It simulates and measures fragmentation for contiguous and linked data."])
+pgClient.query("INSERT INTO projects(title, date, url,languages, info) values($1,$2,$3,$4,$5)",["Migration of Language and Income","2016-05-4","https://github.com/josuerojasrojas/Migration_of_Language_and_Income","Python","This project was for data science course. It tries to show and answer the question ' Is there a relationship between migration vs language vs income?'"])
 //pgClient.query("INSERT INTO projects(title, date, url,languages, info) values($1,$2,$3,$4,$5)",["JukeBox","2017-06-07","https://github.com/josuerojasrojas/JukeBox","HTML, JavaScript, and CSS","This project is a Jukebox that shows basic controls not using the default controls. It can play url mp3s and uses the Spotify to get Spotify preview from song links."])
 
 //-----------------------------------------------------------------------------
