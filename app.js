08dc47c4baf41f5cc2c2a3e7e0c793e7f31092c1
“[josue <[josuerojas.rojas@gmail.com]>
2017-06-23 00:13:56 -0400

    test maps

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ pgClient.query("CREATE TABLE IF NOT EXISTS locations(id SERIAL UNIQUE PRIMARY KE
 //pgClient.query("CREATE TABLE IF NOT EXISTS projects(id SERIAL UNIQUE PRIMARY KEY, title varchar(255) NOT NULL, date date NOT NULL, url varchar(255) NOT NULL, languages varchar(255) NOT NULL, info text NOT NULL)")
 //pgClient.query("INSERT INTO projects(title, date, url,languages, info) values($1,$2,$3,$4,$5)",["Migration of Language and Income","2016-05-4","https://github.com/josuerojasrojas/Migration_of_Language_and_Income","Python","This project was for data science course. It tries to show and answer the question ' Is there a relationship between migration vs language vs income?'"])
 //pgClient.query("INSERT INTO projects(title, date, url,languages, info) values($1,$2,$3,$4,$5)",["JukeBox","2017-06-07","https://github.com/josuerojasrojas/JukeBox","HTML, JavaScript, and CSS","This project is a Jukebox that shows basic controls not using the default controls. It can play url mp3s and uses the Spotify to get Spotify preview from song links."])
-pgClient.query("INSERT INTO locations(id, lat, lng) values(DEFAULT, $1, $2)",[-31.563910,147.154312]);
+//pgClient.query("INSERT INTO locations(id, lat, lng) values(DEFAULT, $1, $2)",[-31.563910,147.154312]);
 //-----------------------------------------------------------------------------
 // home page
 app.get('/', (req, res) => {
